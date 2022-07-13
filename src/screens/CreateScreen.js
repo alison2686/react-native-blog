8d683f061d55900c9727c6e343e3ab91c5a0cac9
Alison Lee <alison2686@gmail.com>
2022-07-12 17:51:25 -0700

    navigate to CreateScreen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native'
+import { Context } from '../context/BlogContext'
+
+const CreateScreen = ({navigation}) => {
+
+  return (
+    <View>
+        <Text>Create Post</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+  })
+
+export default CreateScreen
